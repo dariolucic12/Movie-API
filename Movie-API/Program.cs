608cdfb5d31a9919f70ddc8137dc84ac2f1b147f
Dario Lucić <dlucic@it4.ltd>
2022-06-25 16:15:55 +0200

    added logger logic

--- Movie-API/Program.cs
+++ Movie-API/Program.cs
@@ -1,12 +1,14 @@
 
 using Microsoft.EntityFrameworkCore;
+using Movie_API.Logger;
 using Movie_API.Models;
 using Movie_API.Repository;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -14,6 +16,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddScoped<IWatchlistRepo, WatchlistRepo>();
 builder.Services.AddScoped<IReviewRepo, ReviewRepo>();
+builder.Services.AddSingleton<ILoggerManager, LoggerManager>();
 
 builder.Services.AddDbContext<ApplicationContext>(
     o => o.UseNpgsql(builder.Configuration.GetConnectionString("MovieDb"))
